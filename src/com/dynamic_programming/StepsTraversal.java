167e5a25641e27cd0eb2511b5c74ec024a24941d
Sudamini Shirkhedkar <sudamini.js@gmail.com>
2019-03-25 17:33:46 -0400

    commit

--- src/com/dynamic_programming/StepsTraversal.java
+++ src/com/dynamic_programming/StepsTraversal.java
@@ -1,28 +1,47 @@
 package com.dynamic_programming;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 @EPI
  */
 public class StepsTraversal {
     public static void main(String[] args) {
-        int totalSteps = 4;
-        int maxStepsUCanTake = 2;
+        int totalSteps = 6;
+        int maxStepsUCanTake = 3;
+        Map<Integer, Integer> cache = new HashMap<>();
         System.out.println("Ways to climb " + totalSteps + " = " +
-                totalWays(totalSteps, maxStepsUCanTake, new int[totalSteps + 1]));
+                totalWays(totalSteps, maxStepsUCanTake, cache));
     }
 
-    private static int totalWays(int totalSteps, int maxStepsUCanTake, int[] cache) {
-        if (totalSteps <= 1) {
+    private static int totalWays(int totalSteps, int maxStepsUCanTake, Map<Integer, Integer> map) {
+        System.out.println("Parameters : Total steps =  " + totalSteps + " max steps you can take = " + maxStepsUCanTake +
+                " cache = " + map);
+        if (totalSteps == 2) {
+            return 2;
+        }
+        if (totalSteps == 1) {
             return 1;
         }
-        if (cache[totalSteps] == 0) {
-            for (int i = 1; i <= maxStepsUCanTake && (totalSteps - i) >= 0; i++) {
-                cache[totalSteps] += totalWays(totalSteps - i, maxStepsUCanTake, cache);
-            }
+        if (totalSteps == 0) {
+            return 0;
         }
-        System.out.println("Total ways " + Arrays.toString(cache));
-        return cache[totalSteps];
+        int totalWays = 0;
+        for (int i = 1; i <= maxStepsUCanTake && totalSteps - i > 0; i++) {
+            int val;
+            int param = totalSteps - i;
+            if (map.containsKey(param)) {
+                val = map.get(param);
+            } else {
+                val = totalWays(param, maxStepsUCanTake, map);
+                map.put(param, val);
+            }
+            totalWays += val;
+        }
+        if (totalSteps == maxStepsUCanTake) {
+            totalWays += 1;
+        }
+        return totalWays;
     }
 }
