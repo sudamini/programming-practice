24f50ca16e378bd5ef81b56f320f05d02d71665b
Sudamini Shirkhedkar <sudamini.js@gmail.com>
2019-03-28 20:06:18 -0400

    commit

---
+++ src/com/maths_statistics/PowerOfTen.java
@@ -0,0 +1,31 @@
+package com.maths_statistics;
+
+/*
+Check if a number is power of 10.
+ */
+public class PowerOfTen {
+    public static void main(String[] args) {
+        int n = 100000000;
+        isPowerOfTen(n);
+    }
+
+    private static void isPowerOfTen(int n) {
+        if (n < 10) {
+            System.out.println("Not a power of 10");
+            return;
+        }
+        int div;
+        int num = n;
+        while ((num / 10) > 0) {
+            div = num / 10;
+            num = div;
+        }
+        if (num % 10 == 1) {
+            System.out.println("Number " + n + " is power of 10.");
+        } else {
+            System.out.println("Number " + n + " is not a power of 10.");
+        }
+    }
+
+
+}
