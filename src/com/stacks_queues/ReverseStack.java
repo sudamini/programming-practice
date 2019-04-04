5d5424d12ea61951033fff184084f96495e71e1e
Sudamini Shirkhedkar <sudamini.js@gmail.com>
2019-04-04 11:47:25 -0400

    commit

---
+++ src/com/stacks_queues/ReverseStack.java
@@ -0,0 +1,47 @@
+package com.stacks_queues;
+
+import java.util.Stack;
+
+public class ReverseStack {
+
+    static Stack<Character> stack = new Stack<>();
+
+    public static void main(String[] args) {
+
+        // push elements into
+        // the stack
+        stack.push('1');
+        stack.push('2');
+        stack.push('3');
+        stack.push('4');
+
+        System.out.println("Original Stack");
+        System.out.println(stack);
+
+        reverse();
+
+        System.out.println("Reversed Stack");
+        System.out.println(stack);
+    }
+
+    static void reverse() {
+        if (stack.isEmpty()) {
+            return;
+        }
+        Character c = stack.peek();
+        stack.pop();
+        reverse();
+        insertAtBottom(c);
+    }
+
+    static void insertAtBottom(Character c) {
+        if( stack.isEmpty() ){
+            stack.push(c);
+            return;
+        }
+        Character x = stack.peek();
+        stack.pop();
+        insertAtBottom(x);
+        stack.push(x);
+    }
+}
